deb3d780ce9b3248710bdaa13c0eae66a568be96
coffeecoke <18601376034@163.com>
2018-08-20 17:23:53 +0800

    t0820

---
+++ gulpfile.js
@@ -0,0 +1,204 @@
+var gulp = require('gulp'),
+    $ = require('gulp-load-plugins')({rename: {'gulp-rev': 'rev'}});
+
+var pkg = require('./package.json');
+
+var AUTOPREFIXER_BROWSERS = ["Android >= 4", "Explorer >= 10", "iOS >= 6"];
+
+//注释信息
+var banner = '/*! <%= pkg.title %> v<%= pkg.version %> by YDCSS (c) ' +
+    $.util.date(Date.now(), 'UTC:yyyy') + ' Licensed <%= pkg.license %>' + ' */ \n';
+
+//编译bootstrap
+gulp.task('bootstrap', function () {
+    gulp.src(['bootstrap/less/bootstrap.less'])
+        .pipe($.plumber({errorHandler: $.notify.onError('Error: <%= error.message %>')}))
+        .pipe($.sourcemaps.init())
+        .pipe($.less())
+        .pipe($.autoprefixer({
+            browsers: AUTOPREFIXER_BROWSERS, cascade: false
+        }))
+        .pipe($.sourcemaps.write('./'))
+        .pipe(gulp.dest('bootstrap/css'))
+        .pipe($.livereload());
+});
+
+//编译AdminLET  less的编译
+gulp.task('main-less', function () {
+    gulp.src(['base-less/main.less'])
+        .pipe($.plumber({errorHandler: $.notify.onError('Error: <%= error.message %>')}))
+        .pipe($.sourcemaps.init())
+        .pipe($.less())
+        .pipe($.autoprefixer({
+            browsers: AUTOPREFIXER_BROWSERS, cascade: false
+        }))
+        .pipe($.sourcemaps.write('./'))
+        .pipe(gulp.dest('dist/base-css'))
+        .pipe($.livereload());
+});
+
+
+var componentsOptions = [
+
+    //components
+    {
+        "name" : "bridge-chart",
+        "cssPretreatment" : "less",
+        "src" : ["components/bridge-chart/styles/bridge-chart.less"],
+        "target" : "components/bridge-chart/styles"
+    },
+    {
+        "name" : "weight-chart",
+        "cssPretreatment" : "less",
+        "src" : ["components/weight-chart/styles/weight-chart.less"],
+        "target" : "components/weight-chart/styles"
+    },
+    //pages
+
+    {
+        "name" : "page",
+        "cssPretreatment" : "less",
+        "src" : ["pages/**/*.less"],
+        "target" : "pages"
+    },
+    {
+        "name" : "main",
+        "cssPretreatment" : "less",
+        "src" : ["components/main/main.less"],
+        "target" : "components/main"
+    }
+
+
+]
+
+//components组件预处理器编译,并放入目标文件夹中
+
+gulp.task('components',function(){
+    for(var i=0;i<componentsOptions.length;i++){
+        var cssPretreatment = componentsOptions[i].cssPretreatment
+        if(cssPretreatment=="less"||!cssPretreatment){
+            gulp.src(componentsOptions[i].src)
+                .pipe($.plumber({errorHandler: $.notify.onError('Error: <%= error.message %>')}))
+                .pipe($.sourcemaps.init()) //生成css对应less的映射文件，方便调试
+                .pipe($.less()) 
+                .pipe($.autoprefixer({
+                    browsers: AUTOPREFIXER_BROWSERS, cascade: false
+                }))
+                .pipe($.sourcemaps.write('./'))
+                .pipe(gulp.dest(componentsOptions[i].target))
+                .pipe($.livereload());
+        }else if (cssPretreatment=="sass"){
+
+            gulp.src(componentsOptions[i].src)
+                .pipe($.sass())
+                .pipe(gulp.dest(componentsOptions[i].target))
+        }
+
+    }
+});
+
+//压缩js
+// gulp.task('uglifyJs', function(){
+//     return gulp.src('js-demo/jquery.js')
+//         .pipe($.uglify())
+//         .pipe($.rev())
+//          .pipe(gulp.dest('js-demo'))
+//         .pipe($.rev.manifest())
+//         .pipe(gulp.dest('js-demo'));
+// });
+//gulp.task('concat', function () {
+//    gulp.src(['src/js/source/ydui.js', 'src/js/source/**/*.js'])
+//        .pipe($.concat('ydui.js'))
+//        .pipe(gulp.dest('src/js'));
+//});
+
+//gulp.task('copeJs',function(){
+//    gulp.src('src/js/jquery-2.1.4.min.js')
+//        .pipe(gulp.dest('dist/demo/js'))
+//        .pipe(gulp.dest('dist/build/js'))
+//})
+
+////gulp.task('copeIconfont',function(){
+//    gulp.src('src/iconfont/**')
+//        .pipe(gulp.dest('dist/demo/iconfont'))
+//        .pipe(gulp.dest('dist/build/iconfont'))
+//});
+
+//监控bootstrap
+
+gulp.task('watchBootstrap', function (done) {
+    $.livereload.listen();
+    gulp.watch('bootstrap/**/*.less', ['bootstrap'])
+        .on('end',done);
+});
+
+//监控组件的less变化
+gulp.task('watchComponents-less', function (done) {
+    $.livereload.listen();
+    gulp.watch('components/**/*.less', ['components'])
+        .on('end',done);
+});
+//监控pages的less变化
+gulp.task('watchPages-less', function (done) {
+    $.livereload.listen();
+    gulp.watch('pages/**/*.less', ['components'])
+        .on('end',done);
+});
+
+//监控base-less的变化
+gulp.task('watchBaseLess',function (done) {
+    $.livereload.listen();
+    gulp.watch('base-less/**/*.less', ['main-less'])
+        .on('end',done);
+})
+
+
+gulp.task('build:cssmin', ['main-less'], function () {
+   gulp.src('dist/css/main.css')
+       .pipe($.cleanCss({keepSpecialComments: '*'}))
+       .pipe($.header(banner, {pkg: pkg}))
+       .pipe($.rename({     //重命名
+           suffix: '.min'
+       }))
+       .pipe(gulp.dest('dist/css'));
+});
+
+
+
+//gulp.task('build:uglify', function () {
+//    gulp.src(['src/js/{ydui.js,ydui.flexible.js}'])
+//        .pipe($.uglify())
+//        .pipe($.header(banner, {pkg: pkg}))
+//        .pipe(gulp.dest('dist/build/js'));
+//});
+
+//gulp.task('demo:yoa_css', function () {
+//    gulp.src(['src/less/{ydui,demo}.less']).pipe($.sourcemaps.init())
+//        .pipe($.less())
+//        //.pipe($.cssBase64({
+//        //    extensionsAllowed: ['.ttf']
+//        //}))
+//        .pipe($.autoprefixer({
+//            browsers: AUTOPREFIXER_BROWSERS, cascade: false, remove: true
+//        }))
+//        .pipe($.sourcemaps.write('./'))
+//        .pipe(gulp.dest('dist/demo/yoa_css'))
+//        .pipe(gulp.dest('src/yoa_css'));
+//});
+
+//gulp.task('demo:html', function () {
+//    gulp.src(['src/html/**/*.html', 'src/index.html', 'src/libs/**/*.html'], {base: 'src'})
+//        .pipe($.rev())
+//        .pipe(gulp.dest('dist/demo'));
+//});
+//
+//gulp.task('demo:uglify', function () {
+//    gulp.src(['src/js/{ydui.js,ydui.flexible.js}'])
+//        .pipe(gulp.dest('dist/demo/js'));
+//});
+
+gulp.task('dev', ['bootstrap','main-less','components','watchBootstrap','watchBaseLess','watchComponents-less','watchPages-less']);
+
+//gulp.task('demo', ['demo:yoa_css', 'copeIconfont','copeJs','demo:uglify', 'demo:html']);
+
+gulp.task('default', ['dev']);
---
+++ plugins/echarts-v3/lineChart.js
@@ -0,0 +1,344 @@
+$(function () {
+    var myChart1 = echarts.init(document.getElementById('scatterChart'));
+    var option1 = {
+        // title: {
+        //     text: '对数轴示例',
+        //     left: 'center'
+        // },
+        // '{a} <br/>{b} : {c}'
+        tooltip: {
+            trigger: 'axis',
+            backgroundColor: '#fff',  // 提示背景颜色，默认为透明度为0.7的黑色
+            borderColor: '#ccc',            // 提示边框颜色
+            borderRadius: 4,
+            padding:10,
+            formatter:function(prames){
+                var datas=prames[0].data
+                var datas1=prames[1].data
+                var datas2=prames[2].data
+                return '<div>三月增长趋势</div>'+
+                '<div style="color:#b3b4b4">最高位移量:<span>'+datas+'</span></div>'+
+                '<div style="color:#b3b4b4">最低位移量:<span>'+datas1+'</span></div>'+
+                '<div style="color:#b3b4b4">平均位移量<span>'+datas2+'</span></div>'
+            },
+            textStyle:{
+                color:"#000"
+            },   
+            axisPointer : {            // 坐标git轴指示器，坐标轴触发有效
+                type : 'line',       // 默认为直线，可选为：'line' | 'shadow'
+                backgroundColor:'rgba(239,246,255,0,8)',
+                lineStyle:{
+                    width:80,
+                    // type:"shadow",
+                    shadowColor:"#f7faff",
+                    opacity:0.5,
+                    color: {
+                        type: 'linear',
+                        x: 0,
+                        y: 0,
+                        x2: 0,
+                        y2: 0.9,
+                        colorStops: [{
+                            offset: 0, color: '#f7faff',opacity:'0.1' // 0% 处的颜色
+                        }, {
+                            offset: 1, color: '#e3eeff',opacity:'0.1' // 100% 处的颜色
+                        }],
+                        globalCoord: false // 缺省为 false
+                    }
+                },
+                shadowStyle : {              // 阴影指示器样式设置
+                    width: 'auto',         // 阴影大小
+                    backgroungColor: 'rgba(239,246,255,0,1)'  // 阴影颜色
+                }
+            },
+            extraCssText: 'box-shadow: 0 0 8px rgba(52, 148, 253, 0.3)' ,
+        },
+        color:["#5b85f3","#f8c538","#fd582e"],
+        legend: {
+            show: true,
+            left: 'right',
+            itemWidth:8,
+            itemHeight:8,
+            data: [
+                {name:'最低位移量',icon:'circle'},
+                {name:'最高位移量',icon:'circle'},
+                {name:'平均位移量',icon:'circle'}
+            ]
+        },
+        xAxis: {
+            type: 'category',
+            splitLine: {
+                show: true,
+                lineStyle: {
+                    type: 'dashed'
+                }
+            },
+            data: ['01月', '02月', '03月', '04月', '05月', '06月', '07月', '08月', '09月', "10月", "11月", "12月"]
+        },
+        yAxis: {
+            type: 'value',
+            name: '位移mm',
+            splitLine: {
+                show: true,
+                lineStyle: {
+                    type: 'dashed'
+                }
+            }
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        series: [
+            {
+                name: '最低位移量',
+                type: 'line',
+                data: [20, 23, 15, 27, 21, 30, 25, 50, 40, 70, 50, 40],
+                symbol: 'none',
+                lineStyle: {
+                    normal: {
+                        color: '#f8c538'
+                    }
+                }
+            },
+            {
+                name: '最高位移量',
+                type: 'line',
+                symbol: 'none',
+                data: [25, 30, 20, 38, 23, 42, 30, 60, 50, 80, 120, 140],
+                lineStyle: {
+                    normal: {
+                        color: '#5b85f3'
+                    }
+                }
+            },
+            {
+                name: '平均位移量',
+                type: 'line',
+                symbol: 'none',
+                data: [50, 40, 70, 60, 90, 80, 100, 90, 120, 130, 140, 150],
+                lineStyle: {
+                    normal: {
+                        color: '#fd582e'
+                    }
+                }
+            }
+        ]
+    };
+
+    myChart1.setOption(option1);
+    $(window).resize(function () {
+        myChart1.resize();
+    })
+
+})
+
+
+$(function () {
+    var myChart = echarts.init(document.getElementById('lineChart'));
+    var option = {
+        // title: {
+        //     text: '对数轴示例',
+        //     left: 'center'
+        // },
+        tooltip: {
+            trigger: 'axis',
+            backgroundColor: '#fff',  // 提示背景颜色，默认为透明度为0.7的黑色
+            borderColor: '#ccc',            // 提示边框颜色
+            borderRadius: 4, 
+            padding:10,
+            formatter:function(prames){
+                console.log(prames)
+                var datas=prames[0].data
+                var datas1=prames[1].data
+                var datas2=prames[2].data
+                return '<div>桥梁编号：wy-1-1-5</div>'+
+                '<div style="color:#b3b4b4">偏移量:<span>'+datas+'</span></div>'
+            },
+            textStyle:{
+                color:"#000"
+            },   
+            // formatter: '{a} <br/>{b} : {c}',
+            axisPointer : {            // 坐标git轴指示器，坐标轴触发有效
+                type : 'line',       // 默认为直线，可选为：'line' | 'shadow'
+                backgroundColor:'rgba(239,246,255,0,8)',
+                
+                lineStyle:{
+                    width:100,
+                    // type:"shadow",
+                    shadowColor:"#f7faff",
+                    opacity:0.5,
+                    color: {
+                        type: 'linear',
+                        x: 0,
+                        y: 0,
+                        x2: 0,
+                        y2: 0.9,
+                        colorStops: [{
+                            offset: 0, color: '#f7faff',opacity:'0.1' // 0% 处的颜色
+                        }, {
+                            offset: 1, color: '#e3eeff',opacity:'0.1' // 100% 处的颜色
+                        }],
+                        globalCoord: false // 缺省为 false
+                    }
+                },
+                shadowStyle : {              // 阴影指示器样式设置
+                    width: 'auto',         // 阴影大小
+                    backgroungColor: 'rgba(239,246,255,0,1)'  // 阴影颜色
+                    
+                }
+            },
+            extraCssText: 'box-shadow: 0 0 8px rgba(52, 148, 253, 0.3)' ,            
+        },
+        color:["#5b85f3","#f8c538","#fd582e"],
+        legend: {
+            show: true,
+            left: 'right',
+            itemWidth:8,
+            itemHeight:8,
+            data: [
+                {name:'最低位移量',icon:'circle'},
+                {name:'最高位移量',icon:'circle'},
+                {name:'平均位移量',icon:'circle'}
+            ]
+        },
+        xAxis: {
+            type: 'category',
+            splitLine: {
+                show: true,
+                lineStyle: {
+                    type: 'dashed'
+                }
+            },
+            data: ['19-20', '20-21', '22-23', '24-25', '25-26', '27-28','29-30']
+        },
+        grid: {
+            left: '0',
+            right: '0',
+            bottom: '0',
+            containLabel: true
+        },
+        yAxis: {
+            type: 'value',
+            name: '位移mm',
+            splitLine:{
+                show:true,
+                lineStyle:{
+                    type:'dashed'
+                }
+            },
+        },
+        series: [
+            {
+                name: '最低位移量',
+                type: 'line',
+                symbol: 'none',
+                data: [50, 55, 60, 65, 70, 80,78],
+                lineStyle: {
+                    normal: {
+                        color: '#f8c538'
+                    }
+                }
+            },
+            {
+                name: '最高位移量',
+                type: 'line',
+                symbol: 'none',
+                data: [30, 35, 40, 45, 48, 50,52],
+                lineStyle: {
+                    normal: {
+                        color: '#5b85f3'
+                    }
+                }
+            },
+            {
+                name: '平均位移量',
+                type: 'line',
+                symbol: 'none',
+                data: [15, 20, 25, 30, 40, 40, 42],
+                lineStyle: {
+                    normal: {
+                        color: '#fb582e'
+                    }
+                }
+            }
+        ]
+    };
+    myChart.setOption(option);
+    $(window).resize(function () {
+        myChart.resize();
+    })
+
+})
+$(function () {
+    var Chart1 = echarts.init(document.getElementById('chart1'));
+    var option1 = {
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+                type: 'none'
+            },
+            backgroundColor: '#fff',  // 提示背景颜色，默认为透明度为0.7的黑色
+            borderColor: '#ccc',            // 提示边框颜色
+            borderRadius: 4,
+            padding:10,
+            textStyle:{
+                color:"#000"
+            }, 
+            formatter: function (params) {
+                return "桥梁编号" + ': ' + params[0].name+"<br/>"+
+                        "位移量"+":"+params[0].data
+            },
+            extraCssText: 'box-shadow: 0 0 8px rgba(52, 148, 253, 0.3)' ,            
+        },
+        xAxis: {
+            data: ['WY-1-1-5', ' WY-1-1-5', ' WY-1-1-5', 'WY-1-1-5', 'WY-1-1-5', 'WY-1-1-5'],
+            splitLine: {
+                show: false
+            },
+            axisLabel: {
+                textStyle: {
+                    color: '#c5c5c5'
+                }
+            }
+        },
+        yAxis: {
+            type: 'value',
+            name: '位移mm',
+            splitLine: {
+                show: true,
+                lineStyle: {
+                    type: 'dashed'
+                }
+            }
+        },
+        color: ['#3494fd'],
+        grid: {
+            left: '0',
+            right: '0',
+            bottom: '0',
+            containLabel: true
+        },
+        series: [{
+            name: 'hill',
+            type: 'pictorialBar',
+            barCategoryGap: '-40%',
+            // symbol: 'path://M0,10 L10,10 L5,0 L0,10 z',
+            symbol: 'path://M0,10 L10,10 C5.5,10 5.5,5 5,0 C4.5,5 4.5,10 0,10 z',
+            symbolSize:['100%','100%'],
+            itemStyle: {
+                normal: {
+                    opacity: 0.5
+                },
+                emphasis: {
+                    opacity: 1
+                }
+            },
+            data: [120, 60, 25, 18, 12, 9],
+            z: 10
+        }]
+    };
+    Chart1.setOption(option1);
+})
