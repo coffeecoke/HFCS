a0b1a782fbf601e111471fd3bb75103615d02c07
coffeecoke <18601376034@163.com>
2018-08-21 17:08:07 +0800

    t0821

--- common-js/index.js
+++ common-js/index.js
@@ -1,9 +1,8 @@
 $('.sys-toggle .dropdown-menu li').on('click',function () {
     var logoClass = $(this).attr('class');
-    alert(logoClass)
     $(this).parents('.sys-toggle').find('.header-logo').removeClass().addClass("header-logo "+logoClass)
 })
 
 $('.nav-container .nav > li').on('click',function () {
-    $(this).addClass('navbar-active')
+    $(this).addClass('navbar-active').siblings().removeClass('navbar-active')
 })
