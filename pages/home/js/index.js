2ceff70ebb136c8c61ff99559b3005bb43eb0f0b
coffeecoke <18601376034@163.com>
2018-08-21 19:17:02 +0800

    t0821

--- pages/home/js/index.js
+++ pages/home/js/index.js
@@ -1,12 +1,12 @@
 $(".box-title-fl .nav-list-animate").on("click","li",function(){
     $(this).addClass("active").siblings().removeClass("active");
     var index=$(this).index();
-    var indexfirst=$(this).siblings().first().width();
+    var indexfirst=$(this).siblings().first().outerWidth();
     $(".color-slider").animate({left:indexfirst*index+"px"},300)
 })
 $(".box-title-fr .nav-list-animates").on("click","li",function(){
     $(this).addClass("actives").siblings().removeClass("actives");
     var indexs=$(this).index();
-    var indexfirsts=$(this).siblings().first().width();
+    var indexfirsts=$(this).siblings().first().outerWidth();
     $(".color-sliders").animate({left:indexfirsts*indexs+"px"},300)
 })
