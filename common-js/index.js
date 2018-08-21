4217c6773c11aa432c9ebad7bbe9f175695c8b97
coffeecoke <zhaoqian_0828@163.com>
2018-08-22 00:34:47 +0800

    dchj

--- common-js/index.js
+++ common-js/index.js
@@ -6,10 +6,16 @@ $('.sys-toggle .dropdown-menu li').on('click',function () {
 
 //顶部右侧导航点击效果
 $('.nav-container .nav > li').on('click',function () {
-    $(this).addClass('navbar-active').siblings().removeClass('navbar-active')
+    $(this).parents('.nav-container').find('.navbar-active').removeClass("navbar-active");
+    $(this).addClass("navbar-active")
 })
+
 
 // 左侧导航点击效果
 $('.sidebar-menu .treeview').on('click',function () {
     $(this).addClass('active').siblings().removeClass('active')
+})
+// 下拉框点击不关闭
+$('body').on('click','[data-stopPropagation]',function (e) {
+    e.stopPropagation();
 })
